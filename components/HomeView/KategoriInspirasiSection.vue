7d7c8de0ced0ac5f3393a0d478097b5457ba458d
hafidnurkholis <itsmaehere@gmail.com>
2022-11-26 21:31:49 +0700

    image src fix

--- components/HomeView/KategoriInspirasiSection.vue
+++ components/HomeView/KategoriInspirasiSection.vue
@@ -5,51 +5,51 @@
             :modules="Modules">
             <SwiperSlide>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/dapur.jpg" alt="">
+                    <img src="@/assets/image/category/dapur.jpg" alt="">
                     <p class="p-2 text-xs truncate">Dapur</p>
                 </div>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/fashion_pria.jpg" alt="">
+                    <img src="@/assets/image/category/fashion_pria.jpg" alt="">
                     <p class="p-2 text-xs truncate">Fashion Pria</p>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/handphone_tablet.jpg" alt="">
+                    <img src="@/assets/image/category/handphone_tablet.jpg" alt="">
                     <p class="p-2 text-xs truncate">Handphone & Tablet</p>
                 </div>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/fashion_wanita.jpg" alt="">
+                    <img src="@/assets/image/category/fashion_wanita.jpg" alt="">
                     <p class="p-2 text-xs truncate">Fashion Wanita</p>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/film_musik.jpg" alt="">
+                    <img src="@/assets/image/category/film_musik.jpg" alt="">
                     <p class="p-2 text-xs truncate">Film & Musik</p>
                 </div>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/kamera.jpg" alt="">
+                    <img src="@/assets/image/category/kamera.jpg" alt="">
                     <p class="p-2 text-xs truncate">Kamera</p>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/kosmetik.jpg" alt="">
+                    <img src="@/assets/image/category/kosmetik.jpg" alt="">
                     <p class="p-2 text-xs truncate">Kosmtik</p>
                 </div>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/mainan.jpg" alt="">
+                    <img src="@/assets/image/category/mainan.jpg" alt="">
                     <p class="p-2 text-xs truncate">Hoby & Mainan</p>
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/olahraga.jpg" alt="">
+                    <img src="@/assets/image/category/olahraga.jpg" alt="">
                     <p class="p-2 text-xs truncate">Olahraga</p>
                 </div>
                 <div class="border rounded-md overflow-hidden text-center mb-4">
-                    <img src="../../assets/image/category/otomotif.jpg" alt="">
+                    <img src="@/assets/image/category/otomotif.jpg" alt="">
                     <p class="p-2 text-xs truncate">Otomotif</p>
                 </div>
             </SwiperSlide>
--- components/HomeView/SpecialHariIniSection.vue
+++ components/HomeView/SpecialHariIniSection.vue
@@ -2,10 +2,10 @@
     <div>
         <h3 class="font-semibold m-5">Special di Tokopedia hari ini</h3>
         <div class="grid grid-cols-2 grid-rows-2 gap-1">
-            <img src="../../assets/image/banner/banner_special (1).jpg" alt="">
-            <img src="../../assets/image/banner/banner_special (2).jpg" alt="">
-            <img src="../../assets/image/banner/banner_special (3).jpg" alt="">
-            <img src="../../assets/image/banner/banner_special (4).jpg" alt="">
+            <img src="@/assets/image/banner/banner_special (1).jpg" alt="">
+            <img src="@/assets/image/banner/banner_special (2).jpg" alt="">
+            <img src="@/assets/image/banner/banner_special (3).jpg" alt="">
+            <img src="@/assets/image/banner/banner_special (4).jpg" alt="">
         </div>
     </div>
 </template>
