5847f4f3714fa597f86c7955b3f6608d934c3e99
hafidnurkholis <itsmaehere@gmail.com>
2022-11-26 21:27:57 +0700

    for you section

---
+++ components/HomeView/ForYouSection.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="bg-gray-100">
+        <div class="flex flex-wrap">
+            <div v-for="item in foryou_product" :key="item.id" class="w-1/2 p-2 ">
+                <ProductCard :product="item" />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+const foryou_product = await useFetch('https://fakestoreapi.com/products?sort=desc').then(res => {
+    return res.data
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
