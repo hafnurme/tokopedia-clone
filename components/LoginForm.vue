b2f402f946cdfc9023e0f66d5a1205294ed3df3d
hafidnurkholis <itsmaehere@gmail.com>
2022-11-28 19:18:06 +0700

    add login page

---
+++ components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="border h-screen">
+        <div class="flex items-center justify-between p-5">
+            <h3 class="flex items-center font-bold"><span class="material-icons mr-2">arrow_back_ios</span>Masuk</h3>
+            <p class="text-primary">Daftar </p>
+        </div>
+        <div class="px-5">
+            <div class="py-3">
+                <label>No HP atau Email</label>
+                <input type="text" class="border-b outline-none w-full p-3 mb-2">
+                <p class="text-end text-sm text-primary">Butuh bantuan ?</p>
+            </div>
+            <div class="mb-2">
+                <button class="bg-slate-200 rounded-lg w-full p-3 text-slate-400 font-bold">Selanjutnya</button>
+            </div>
+            <div class="flex items-center my-4">
+                <hr class="flex-1">
+                <span class="flex-1 text-center">atau dengan</span>
+                <hr class="flex-1">
+            </div>
+            <div class="mb-2">
+                <button class="border rounded-lg w-full p-3   font-bold">Google</button>
+            </div>
+            <div class="text-center">
+                <p>Belum punya akun? <span class="text-primary">Daftar</span></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
