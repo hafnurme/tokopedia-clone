ac6c4ca45282b63932f4fb189989f3b7670e8048
hafidnurkholis <itsmaehere@gmail.com>
2022-11-30 14:22:17 +0700

    small update

--- components/Header.vue
+++ components/Header.vue
@@ -4,10 +4,10 @@
             <input type="text" placeholder="Cari di Tokopedia"
                 class="rounded-md border-2 outline-none placeholder:text-sm py-1 px-4 w-7/12">
             <div class="text-white flex justify-between flex-1 ml-3">
-                <i class="material-icons">email</i>
-                <i class="material-icons">notifications</i>
-                <i class="material-icons">shopping_cart</i>
-                <i class="material-icons">menu</i>
+                <i class="material-icons" @click="router.push({ path: '/login' })">email</i>
+                <i class="material-icons" @click="router.push({ path: '/login' })">notifications</i>
+                <i class="material-icons" @click="router.push({ path: '/login' })">shopping_cart</i>
+                <i class="material-icons" @click="router.push({ path: '/login' })">menu</i>
             </div>
         </nav>
         <div class="flex text-white text-sm items-center py-2" v-if="location_on">
@@ -21,16 +21,22 @@
 <script setup>
 const nav = ref(null)
 const location_on = ref(true)
+const router = useRouter();
+const wheelie = () => {
+    if (window.scrollY > 10) {
+        nav.value.classList.add('nav-scrolled')
+        location_on.value = false
+    } else {
+        nav.value.classList.remove('nav-scrolled')
+        location_on.value = true
+    }
+}
 onMounted(() => {
-    document.body.addEventListener('wheel', () => {
-        if (window.scrollY > 10) {
-            nav.value.classList.add('nav-scrolled')
-            location_on.value = false
-        } else {
-            nav.value.classList.remove('nav-scrolled')
-            location_on.value = true
-        }
-    })
+    document.body.addEventListener('wheel', wheelie)
+})
+
+onUnmounted(() => {
+    document.body.removeEventListener('wheel', wheelie)
 })
 
 </script>
--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="flex flex-col bg-white rounded-md overflow-hidden w-full h-[236px] drop-shadow border">
+    <div class="flex flex-col bg-white rounded-md overflow-hidden w-full h-[236px] drop-shadow border"
+        @click="handleProduct(product.id)">
         <div class="flex items-center justify-center h-32">
             <img :src="product.image" alt="" class="max-h-[6rem]">
         </div>
@@ -16,6 +17,12 @@
 
 <script setup>
 defineProps(['product'])
+const router = useRouter()
+const handleProduct = (id) => {
+    router.push({
+        path: `product/${id}`
+    })
+}
 </script>
 
 <style lang="scss" scoped>
